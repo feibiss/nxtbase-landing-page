396dc537156ca955ffe79fa9ea257537fa5573f1
Sreejith K <srejitk@gmail.com>
2023-12-16 00:32:20 +0530

    Style : Updated Header styles

--- landing-1/src/components/containers/header.tsx
+++ landing-1/src/components/containers/header.tsx
@@ -6,52 +6,65 @@ import {
 } from "@/components/ui/navigation-menu";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { HamburgerMenuIcon } from "@radix-ui/react-icons";
+import Image from "next/image";
 
 export default function Header() {
   return (
-    <header className="flex h-20 w-full shrink-0 items-center px-4 md:px-6 sticky top-0 bg-opacity-90 backdrop-blur-lg border-b border-gray-200">
-      <Link className="mr-6 hidden lg:flex" href="#">
-        <span className="sr-only">Acme Inc</span>
+    <header className="flex bg-white/80 left-0 h-20 w-full shrink-0 items-center z-50 px-4 md:px-6 sticky top-0 bg-opacity-90 backdrop-blur-md border-b border-gray-200">
+      <Link
+        className="mr-6 w-fit h-full lg:flex justify-start items-center"
+        href="#"
+      >
+        <span className="sr-only">Nextbase</span>
+        <Image alt="brand-logo" width={100} height={64} src="/vercel.svg" />
       </Link>
-      <NavigationMenu className="hidden lg:flex">
+
+      <NavigationMenu className="hidden md:flex">
         <NavigationMenuList>
           <NavigationMenuLink asChild>
-            <Link
-              className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
-              href="#"
-            >
-              Home
-            </Link>
+            <NavMenuItem href="#" content="Home" />
           </NavigationMenuLink>
           <NavigationMenuLink asChild>
-            <Link
-              className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
-              href="#"
-            >
-              Pricing
-            </Link>
+            <NavMenuItem href="#" content="Pricing" />
           </NavigationMenuLink>
           <NavigationMenuLink asChild>
-            <Link
-              className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
-              href="#"
-            >
-              Features
-            </Link>
+            <NavMenuItem href="#" content="Features" />
           </NavigationMenuLink>
           <NavigationMenuLink asChild>
-            <Link
-              className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
-              href="#"
-            >
-              Contact
-            </Link>
+            <NavMenuItem href="#" content="Contact" />
           </NavigationMenuLink>
         </NavigationMenuList>
       </NavigationMenu>
       <div className="ml-auto">
         <Button>Get Started</Button>
+        {/* // TODO Add Mobile Navbar */}
+        <Button
+          size={"icon"}
+          variant={"outline"}
+          rounded={"full"}
+          className="ml-4"
+        >
+          <HamburgerMenuIcon />
+        </Button>
       </div>
     </header>
   );
 }
+
+const NavMenuItem = ({
+  href = "#",
+  content,
+}: {
+  href: string;
+  content: string;
+}) => {
+  return (
+    <Link
+      className="group inline-flex h-9 w-max items-center justify-center rounded-md underline decoration-2 decoration-transparent hover:decoration-primary underline-offset-8 px-4 py-2 text-sm font-medium transition-colors   hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
+      href={href}
+    >
+      {content}
+    </Link>
+  );
+};
