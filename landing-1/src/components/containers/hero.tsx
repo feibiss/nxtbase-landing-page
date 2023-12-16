7ad29843c10fa7ce58c3d9a1f655e550566f65e1
Sreejith K <srejitk@gmail.com>
2023-12-16 23:21:37 +0530

    Style : Minor changes to hero

--- landing-1/src/components/containers/hero.tsx
+++ landing-1/src/components/containers/hero.tsx
@@ -1,22 +1,35 @@
-import { Button } from "@/components/ui/button";
 import { Avatar } from "@/components/ui/avatar";
+import { Button } from "../ui/button";
 
 export default function Hero() {
   return (
-    <div className="bg-white text-center p-12">
-      <div className="inline-block bg-[#00f2fe] text-[#0033cc] rounded-full px-3 py-1 text-xs font-semibold uppercase tracking-wider">
-        New
+    <div className="text-center p-12 flex flex-col items-center justify-center">
+      <div className="absolute inset-0 -z-10 h-full w-full bg-white [background:radial-gradient(100%_100%_at_50%_10%,#fff_40%,#60a5fa_120%)]"></div>
+      <div className="w-fit flex items-center justify-center rounded-full p-1 gap-2 border-2 border-primary/20">
+        <div className="inline-block bg-primary text-white border-2 border-indigo-400/20 rounded-full px-3 py-1 text-xs font-semibold uppercase tracking-wider">
+          New
+        </div>
+        <p className="mr-2 text-primary font-semibold ">
+          Latest release related information.
+        </p>
       </div>
-      <h1 className="mt-4 text-5xl font-extrabold text-gray-900 sm:text-6xl">
+      <h1 className="mt-4 text-5xl max-w-[80%] font-extrabold text-gray-900 sm:text-6xl">
         The only LinkedIn content creation tool you’ll ever need
       </h1>
-      <p className="mt-4 max-w-xl mx-auto text-base text-gray-500">
+      <p className="mt-8 max-w-xl mx-auto text-base text-gray-500">
         Generate content ideas, high-quality LinkedIn posts, make eye-catching
         carousels, format for readability, and schedule easily—all in one place!
       </p>
-      <Button className="mt-8 bg-blue-500 text-white px-8 py-3 rounded-full shadow-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">
-        Get started for free
-        <ArrowRightIcon className="ml-2 w-4 h-4" />
+      <Button
+        className="rounded-full bg-primary cursor-pointer mt-8 p-0"
+        size={"lg"}
+      >
+        <div className="flex w-full justify-between gap-4 items-center px-1 py-4">
+          <p className="pl-4">Get started for free</p>
+          <div className="bg-white rounded-full p-2 h-8 w-8 text-primary flex justify-center items-center">
+            <ArrowRightIcon className="w-4 h-4" />
+          </div>
+        </div>
       </Button>
       <div className="mt-6 flex justify-center space-x-2">
         <CheckCircleIcon className="w-6 h-6 text-green-500" />
@@ -32,8 +45,8 @@ export default function Hero() {
         <Avatar alt="User 5" src="/placeholder.svg?height=32&width=32" />
         <Avatar alt="User 6" src="/placeholder.svg?height=32&width=32" />
       </div>
-      <div className="mt-2 flex justify-center items-center">
-        <StarIcon className="w-5 h-5 text-yellow-400" />
+      <div className="mt-2 flex justify-center items-center group">
+        <StarIcon className="w-5 h-5 text-yellow-400 hover:fill-yellow-400" />
         <StarIcon className="w-5 h-5 text-yellow-400" />
         <StarIcon className="w-5 h-5 text-yellow-400" />
         <StarIcon className="w-5 h-5 text-yellow-400" />
