5a5c6fe6880de4e080fcf50b72ad317bea8c1ede
Sreejith K <srejitk@gmail.com>
2023-12-17 02:00:26 +0530

    Style : Added animation for carousal

--- landing-1/tailwind.config.ts
+++ landing-1/tailwind.config.ts
@@ -65,10 +65,21 @@ const config: Config = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
+        "infinite-scroll": {
+          from: { transform: "translateX(0)" },
+          to: { transform: "translateX(-100%)" },
+        },
+        "reverse-infinite-scroll": {
+          from: { transform: "translateX(-100%)" },
+          to: { transform: "translateX(0%)" },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        "infinite-scroll": "infinite-scroll 25s linear infinite",
+        "reverse-infinite-scroll":
+          "reverse-infinite-scroll 25s linear infinite",
       },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
