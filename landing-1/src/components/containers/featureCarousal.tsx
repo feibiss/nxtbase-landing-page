8ee8a0148ed638c85abce8b313f9fa2253d56292
Sreejith K <srejitk@gmail.com>
2023-12-18 18:54:35 +0530

    Refactor : Made the carousal structure modular

--- landing-1/src/components/containers/featureCarousal.tsx
+++ landing-1/src/components/containers/featureCarousal.tsx
@@ -1,5 +1,10 @@
+import clsx from "clsx";
+
+type Props = {
+  direction: "reverse" | "default";
+};
 export default function FeatureCarousel() {
-  const logos = [
+  const FEATURES = [
     { src: "LinkedIn Carousalling", alt: "Facebook" },
     { src: "Add Emoji", alt: "Disney" },
     { src: "Posts for you", alt: "Airbnb" },
@@ -11,39 +16,45 @@ export default function FeatureCarousel() {
   ];
 
   return (
-    <div className="flex flex-col items-start justify-start gap-8">
+    <div className="flex flex-col items-start justify-start gap-12 max-w-screen w-screen overflow-hidden mb-8">
       <div className="w-full inline-flex flex-nowrap overflow-hidden [mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-128px),transparent_100%)]">
-        <ul className="flex items-center justify-center md:justify-start gap-6 will-change-transform mx-4 animate-infinite-scroll">
-          {logos.map((logo, index) => (
-            <li
-              className="min-w-[12rem] cursor-pointer hover:border-primary/20 hover:text-primary hover:bg-primary-foreground border-4 border-transparent py-4 bg-gray-100 w-full font-medium rounded-full flex-nowrap flex justify-center items-center"
-              key={index}
-            >
-              {logo.src}
-            </li>
-          ))}
-        </ul>
-        <ul className="flex items-center justify-center md:justify-start gap-6 will-change-transform mx-4 animate-infinite-scroll">
-          {logos.map((logo, index) => (
-            <li
-              className=" min-w-[12rem] cursor-pointer hover:border-primary/20 hover:text-primary hover:bg-primary-foreground border-4 border-transparent py-4 bg-gray-100 w-full font-medium rounded-full flex-nowrap flex justify-center items-center"
-              key={index}
-            >
-              {logo.src}
-            </li>
-          ))}
-        </ul>
-        <ul className="flex items-center justify-center md:justify-start gap-6 will-change-transform mx-4 animate-infinite-scroll">
-          {logos.map((logo, index) => (
-            <li
-              className="min-w-[12rem] cursor-pointer hover:border-primary/20 hover:text-primary hover:bg-primary-foreground border-4 border-transparent py-4 bg-gray-100 w-full font-medium rounded-full flex-nowrap flex justify-center items-center"
-              key={index}
-            >
-              {logo.src}
-            </li>
-          ))}
-        </ul>
+        <AnimatingList features={FEATURES} />
+        <AnimatingList features={FEATURES} />
+        <AnimatingList features={FEATURES} />
+      </div>
+      <div className="w-full inline-flex flex-nowrap overflow-hidden [mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-128px),transparent_100%)]">
+        <AnimatingList features={FEATURES} direction="reverse" />
+        <AnimatingList features={FEATURES} direction="reverse" />
+        <AnimatingList features={FEATURES} direction="reverse" />
       </div>
     </div>
   );
 }
+
+const AnimatingList = ({
+  features,
+  direction = "default",
+}: {
+  features: { src: string }[];
+  direction?: "reverse" | "default";
+}) => {
+  return (
+    <ul
+      className={clsx(
+        "flex items-center justify-center md:justify-start gap-6 will-change-transform mx-4 py-4",
+        direction === "default"
+          ? "animate-infinite-scroll"
+          : "animate-reverse-infinite-scroll"
+      )}
+    >
+      {features.map((feat: { src: string }) => (
+        <li
+          className="min-w-[12rem] cursor-pointer bg-gradient-to-t from-gray-100 to-transparent bg-white hover:text-primary/70 hover:from-primary/20   shadow-[2px_-1px_1px_1px,-2px_2px_0px_1px] shadow-gray-300/40 hover:shadow-primary/20  py-4  w-full font-medium rounded-full flex-nowrap flex justify-center items-center"
+          key={feat.src}
+        >
+          {feat.src}
+        </li>
+      ))}
+    </ul>
+  );
+};
