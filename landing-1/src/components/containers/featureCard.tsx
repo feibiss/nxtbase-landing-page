4d27c9d8773882f1d930aceb41d37c8dfb1f22de
Sreejith K <srejitk@gmail.com>
2023-12-19 02:56:13 +0530

    Style : Added Responsiveness

--- landing-1/src/components/containers/featureCard.tsx
+++ landing-1/src/components/containers/featureCard.tsx
@@ -1,6 +1,8 @@
-import { Button } from "@/components/ui/button";
+import { Button, buttonVariants } from "@/components/ui/button";
 import { CardContent, Card } from "@/components/ui/card";
 import { CheckIcon } from "@radix-ui/react-icons";
+import clsx from "clsx";
+import Link from "next/link";
 
 type FeatureType = {
   title: string;
@@ -12,9 +14,9 @@ type FeatureType = {
 };
 export default function FeatureCard({ feature }: { feature: FeatureType }) {
   return (
-    <div className="relative w-full mx-auto px-28 py-8 group odd:bg-gray-50 last:mb-20">
+    <div className="relative w-full mx-auto md:px-28 md:py-8 py-4 group odd:bg-gray-50 last:mb-20">
       <div className="absolute inset-0 bg-gradient-to-br from-skyblue to-transparent" />
-      <Card className="relative z-10 w-full bg-inherit border-none shadow-none p-8 flex items-center col-span-2">
+      <Card className="relative z-10 w-full bg-inherit border-none shadow-none p-8 flex md:flex-row flex-col items-center col-span-2">
         <CardContent className={"group-odd:order-1 group-even:order-2"}>
           <h3 className="text-base font-semibold mb-2 uppercase text-blue-500 mt-0">
             {feature.subtitle}
@@ -29,21 +31,28 @@ export default function FeatureCard({ feature }: { feature: FeatureType }) {
               </li>
             ))}
           </ul>
-          <Button
-            className="rounded-full border-blue-200 bg-white"
-            variant="outline"
+          <Link
+            href={"#"}
+            className={clsx(
+              buttonVariants({
+                variant: "outline",
+                size: "sm",
+                rounded: "full",
+              }),
+              "text-xs px-2 rounded-full m-0 border-2 border-blue-400 "
+            )}
           >
             Learn More
-          </Button>
+          </Link>
         </CardContent>
-        <div className="md:order-1 w-full h-full col-span-1">
+        <div className="md:order-1 w-full h-full col-span-1 md:mb-0 mb-8">
           <video
             autoPlay
             loop
             muted
             width={500}
             height={300}
-            className="rounded-lg shadow-lg min-w-[500px] mt-8 min-h-[300px] aspect-[500/300] object-cover object-center bg-gray-300"
+            className="rounded-lg shadow-lg min-w-[300px] md:min-w-[500px] mt-8 min-h-[300px] aspect-[500/300] object-cover object-center bg-gray-300"
             src={feature.imgSrc}
             preload=""
           ></video>
