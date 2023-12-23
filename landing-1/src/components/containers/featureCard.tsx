45a21bc2a503282676d7688a5c80f3d680465156
Sreejith K <srejitk@gmail.com>
2023-12-24 02:03:41 +0530

    Style : Removed extra paddings

--- landing-1/src/components/containers/featureCard.tsx
+++ landing-1/src/components/containers/featureCard.tsx
@@ -3,6 +3,7 @@ import { CardContent, Card } from "@/components/ui/card";
 import { CheckIcon } from "@radix-ui/react-icons";
 import clsx from "clsx";
 import Link from "next/link";
+import { Typography } from "../ui/typography";
 
 type FeatureType = {
   title: string;
@@ -14,18 +15,27 @@ type FeatureType = {
 };
 export default function FeatureCard({ feature }: { feature: FeatureType }) {
   return (
-    <div className="relative w-full mx-auto md:px-28 md:py-8 py-4 group odd:bg-secondary last:mb-20">
-      <div className="absolute inset-0 bg-gradient-to-br from-skyblue to-transparent" />
-      <Card className="relative z-10 w-full bg-inherit border-none shadow-none p-8 flex md:flex-row flex-col items-center col-span-2">
+    <div className="relative w-full mx-auto md:px-28 md:py-8 py-4 group odd:bg-accent/50 last:mb-20">
+      <Card className="relative  z-10 w-full border-none shadow-none bg-transparent flex md:flex-row flex-col items-start col-span-2">
         <CardContent className={"group-odd:order-1 group-even:order-2"}>
-          <h3 className="text-base font-semibold mb-2 uppercase text-blue-500 mt-0">
+          <Typography
+            variant={"lead"}
+            className="text-sm font-bold mb-2 uppercase tracking-widest text-primary/80 mt-0"
+          >
             {feature.subtitle}
-          </h3>
-          <h2 className="text-4xl font-bold mb-4">{feature.title}</h2>
-          <p className="text-lg mb-6">{feature.desc}</p>
-          <ul className="list-none space-y-2 mb-6">
+          </Typography>
+          <Typography variant={"h3"} className="">
+            {feature.title}
+          </Typography>
+          <Typography className="text-pretty text-secondary-foreground mb-2">
+            {feature.desc}
+          </Typography>
+          <ul className="list-none space-y-2 my-2">
             {feature.features?.map((feature) => (
-              <li key={feature} className="flex items-center">
+              <li
+                key={feature}
+                className="flex items-center text-secondary-foreground"
+              >
                 <CheckIcon className="text-green-500 mr-2 h-6 w-6 capitalize" />
                 {feature}
               </li>
@@ -45,14 +55,14 @@ export default function FeatureCard({ feature }: { feature: FeatureType }) {
             Learn More
           </Link>
         </CardContent>
-        <div className="md:order-1 w-full h-full col-span-1 flex justify-center items-center md:mb-0 mb-8">
+        <div className="md:order-1 pt-6 w-full h-full col-span-1 flex justify-center items-center">
           <video
             autoPlay
             loop
             muted
-            width={500}
-            height={300}
-            className="rounded-lg shadow-lg min-w-[300px] md:min-w-[500px] mt-8 min-h-[300px] aspect-[500/300] object-cover object-center bg-gray-300"
+            width={600}
+            height={400}
+            className="rounded-lg shadow-lg min-w-[400px] md:min-w-[600px] min-h-[400px] aspect-[600/400] object-cover object-center bg-gray-300"
             src={feature.imgSrc}
             preload=""
           ></video>
