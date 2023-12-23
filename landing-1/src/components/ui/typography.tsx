aa80d7d9acea5bf0f503010bc059b794e20c5756
Sreejith K <srejitk@gmail.com>
2023-12-24 02:04:54 +0530

    Style : Updated text sizes

--- landing-1/src/components/ui/typography.tsx
+++ landing-1/src/components/ui/typography.tsx
@@ -7,9 +7,9 @@ const typographyVariants = cva("text-foreground", {
   variants: {
     variant: {
       h1: "scroll-m-20 text-5xl font-semibold tracking-tight lg:text-7xl",
-      h2: "scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0",
-      h3: "scroll-m-20 text-2xl font-semibold tracking-tight",
-      h4: "scroll-m-20 text-xl font-semibold tracking-tight",
+      h2: "scroll-m-20 border-b pb-2 text-4xl font-semibold tracking-tight first:mt-0",
+      h3: "scroll-m-20 text-3xl font-semibold tracking-tight",
+      h4: "scroll-m-20 text-2xl font-semibold tracking-tight",
       h5: "scroll-m-20 text-lg font-semibold tracking-tight",
       h6: "scroll-m-20 text-base font-semibold tracking-tight",
       p: "leading-7 [&:not(:first-child)]:mt-6",
