ebaf02192ddc8890beb337281bd7e9fb0d0268b9
Sreejith K <srejitk@gmail.com>
2023-12-24 02:13:43 +0530

    Chore : removed fluff

--- landing-1/src/lib/enums.ts
+++ landing-1/src/lib/enums.ts
@@ -7,7 +7,7 @@ export const TESTIMONIES = [
     profilePic:
       "https://res.cloudinary.com/dkqrmlxlg/image/upload/v1702967050/usenextbase/Avatars/testimony_1.webp",
     review:
-      "We've tried numerous SaaS solutions, but none compare to Company Z's platform. Its seamless integration with our existing systems was a breeze. As our business expanded, the scalability of this SaaS became evident – it effortlessly grew with us. The flexibility it offers has been crucial in adapting to changing market demands. Highly recommend it to any business aiming for growth and adaptability.",
+      "We've tried numerous SaaS solutions, but none compare to Company Z's platform. As our business expanded, the scalability of this SaaS became evident – it effortlessly grew with us. Highly recommend it to any business aiming for growth and adaptability.",
   },
   {
     author: "James Parr",
@@ -17,7 +17,7 @@ export const TESTIMONIES = [
     profilePic:
       "https://res.cloudinary.com/dkqrmlxlg/image/upload/v1702967047/usenextbase/Avatars/testimony_3.webp",
     review:
-      "Implementing SaaS from Company Y has been a game-changer for us. Not only has it streamlined our processes, but it has also significantly boosted our ROI. The analytics tools provided have allowed us to make data-driven decisions, leading to smarter strategies and increased profitability. Plus, the regular updates and new features constantly add value to our subscription.",
+      "Implementing SaaS from Company Y has been a game-changer for us. The analytics tools provided have allowed us to make data-driven decisions, leading to smarter strategies and increased profitability. Plus, the regular updates and new features constantly add value to our subscription.",
   },
   {
     author: "Michelle Dakota",
@@ -27,7 +27,7 @@ export const TESTIMONIES = [
     profilePic:
       "https://res.cloudinary.com/dkqrmlxlg/image/upload/v1702967097/usenextbase/Avatars/testimony_2.webp",
     review:
-      "Company X's SaaS has completely transformed the way we operate. With its intuitive interface and powerful features, our team's productivity has soared. Tasks that used to take hours are now accomplished in minutes. The customer support is exceptional, always responsive and helpful. Our business wouldn't be where it is today without this game-changing solution.",
+      "Company X's SaaS has completely transformed the way we operate. Tasks that used to take hours are now accomplished in minutes. Our business wouldn't be where it is today without this game-changing solution.",
   },
 ];
 
@@ -59,14 +59,11 @@ export const SHOWCASE_FEATURES = [
     title: "Built in user authentication system",
     subtitle: "User Authentication",
     navigateTo: "#",
-    desc: "Nextbase supports a wide range of authentication providers such as Google, GitHub, Twitter, Facebook, Apple, Discord and many more. It also provides the flexibility for users to sign in with their email and password or even just with their email. The built-in functionality includes features like forgot password and updating email, making it a comprehensive user authentication system.",
+    desc: "Nextbase supports a wide range of authentication providers such as Google, GitHub, Twitter, Facebook, Apple, Discord and many more. It also provides the flexibility for users to sign in with their email and password or even just with their email.",
     features: [
       "Multi-provider authentication support",
       "Flexible sign-in options: Email and password, or solely via email",
       "Comprehensive user authentication system within Nextbase",
-      'Built-in functionality includes "Forgot Password" feature for user convenience',
-      "Ability for users to update their email within the system",
-      "Seamless integration with various authentication providers for user flexibility",
     ],
     imgSrc:
       "https://res.cloudinary.com/dkqrmlxlg/video/upload/v1702921481/usenextbase/tinywow_signup_43386486_t9iuny.webm",
@@ -75,13 +72,10 @@ export const SHOWCASE_FEATURES = [
     title: "Built in Admin Panel",
     subtitle: "Admin Panel",
     navigateTo: "#",
-    desc: "Nextbase comes with a built-in admin panel that allows you to have a secret area within your app where you can manage users, teams, organizations and more. It provides a comprehensive and intuitive interface for managing all aspects of your application, from users to projects and organizations. This feature is designed to make administrative tasks easier and more efficient.",
+    desc: "Nextbase comes with a built-in admin panel that allows you to have a secret area within your app where you can manage users, teams, organizations and more. It provides a comprehensive and intuitive interface for managing all aspects of your application, from users to projects and organizations.",
     features: [
       "Integrated admin panel for confidential app management",
       "Secret area within the application for admin access.",
-      "Manages users, teams, organizations, and additional functionalities.",
-      "Comprehensive interface for streamlined app management.",
-      "Covers various aspects: users, projects, and organizations.",
     ],
     imgSrc:
       "https://res.cloudinary.com/dkqrmlxlg/video/upload/v1702915076/usenextbase/tinywow_admin-panel-gif_43379830_tltcqi.webm",
@@ -93,13 +87,10 @@ export const SHOWCASE_FEATURES = [
     features: [
       "User Impersonation, a potent debugging tool, integrated into the admin panel",
       "Ability to mimic and view the app from any user's perspective",
-      "Enables thorough debugging of user-facing issues directly within the app",
-      "Particularly useful for troubleshooting complex or hard-to-reproduce problems",
-      "Provides a real-time view of user experience, aiding in issue identification and resolution",
-      "Facilitates understanding and replicating user problems for effective debugging",
-      "Streamlines the debugging process by offering a direct insight into user interactions",
+      "Enables thorough debugging of user-facing issues",
+      "Particularly useful for troubleshooting complex problems",
     ],
-    desc: "Admin panel also comes with a powerful debugging feature called User Impersonation. This allows you to impersonate any user in your app and see what they see. This is a great way to debug issues that your users are facing.Some issues are hard to reproduce and this feature will help you debug the issues that your users are facing and fix them.",
+    desc: "Admin panel also comes with a powerful debugging feature called User Impersonation. This allows you to impersonate any user in your app and see what they see.Some issues are hard to reproduce and this feature will help you debug the issues and fix them.",
     imgSrc:
       "https://res.cloudinary.com/dkqrmlxlg/video/upload/v1702921499/usenextbase/tinywow_user-imp_43386586_xpo6pr.webm",
   },
@@ -109,11 +100,8 @@ export const SHOWCASE_FEATURES = [
     navigateTo: "#",
     features: [
       "Leverages Next.js 13's latest features: app folder, layouts, server components, and server-side rendering",
-      "Optimization of data fetching for enhanced performance and responsiveness.",
-      "Focuses on delivering exceptional user experiences through advanced Next.js capabilities.",
-      "Empowers developers to build high-performing applications.",
-      "Utilizes cutting-edge Next.js features to meet the evolving demands of modern users.",
-      "Enhances speed and efficiency in rendering and serving content.",
+      "Optimization of data fetching for enhanced performance.",
+      "Exceptional user experiences through advanced Next.js capabilities.",
     ],
     desc: "Nextbase is designed to deliver incredible performance. It leverages Next.js 13's all-new app folder, layouts, server components, and server-side rendering to optimize data fetching and provide the best user experience. With Nextbase, you can build high-performance applications that meet the demands of today's users.",
     imgSrc:
@@ -124,14 +112,11 @@ export const SHOWCASE_FEATURES = [
     subtitle: "Simplified Payment flow",
     navigateTo: "#",
     features: [
-      "Pre-configured integration with Stripe, a prominent payment processing platform",
-      "Synchronization between the Nextbase database and customer activities in Stripe facilitated through Webhooks",
-      "Real-time updates on customer activities reflected in the Nextbase database via Stripe Webhooks",
-      "Seamless integration ensures automatic frontend updates based on changes made in Stripe",
-      "Simplifies product creation and pricing updates in Stripe, automatically reflected in the frontend",
-      "Provides a seamless user experience by keeping the frontend in sync with Stripe changes",
+      "Pre-configured integration with Stripe",
+      "Synchronization between the Nextbase database and stripe",
+      "Real-time updates on customer activities via Stripe Webhooks",
     ],
-    desc: "Nextbase is pre-configured with Stripe, a leading online payment processing platform. Your database will be in sync with all customer activity in Stripe via Webhooks. This means that when you create new products or update pricing in Stripe, your frontend will automatically reflect the new pricing, providing a seamless experience for your users.",
+    desc: "Nextbase is pre-configured with Stripe, a leading online payment processing platform. Your database will be in sync with all customer activity in Stripe via Webhooks. ",
     imgSrc:
       "https://res.cloudinary.com/dkqrmlxlg/video/upload/v1702916692/usenextbase/tinywow_stripe-payments-gif_43381706_by2uhy.webm",
   },
