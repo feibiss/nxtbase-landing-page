e5acdd99e45a38d0ae727424d2cb07679deb6ef2
Sreejith K <srejitk@gmail.com>
2023-12-24 02:04:22 +0530

    Style : Changed the card styling + spacings

--- landing-1/src/components/containers/testimonials.tsx
+++ landing-1/src/components/containers/testimonials.tsx
@@ -1,15 +1,16 @@
 import { AvatarImage, AvatarFallback, Avatar } from "@/components/ui/avatar";
-import { Card } from "@/components/ui/card";
+import { Card, CardContent, CardFooter } from "@/components/ui/card";
 import { TESTIMONIES } from "@/lib/enums";
+import { Typography } from "../ui/typography";
 
 export default function Testimonials() {
   return (
-    <section className="container snap-start w-full py-12 md:py-24 lg:py-32">
-      <div className="mx-auto px-4 md:px-6 lg:px-8 text-center bg-background">
-        <h2 className="text-3xl text-foreground flex justify-center items-center text-center pb-4 font-bold tracking-tighter sm:text-4xl md:text-5xl mb-20">
+    <section className="snap-start w-full max-w-6xl">
+      <div className="mx-auto text-center bg-background">
+        <Typography variant={"h2"} className="prose text-pretty">
           What Our Users Are Saying
-        </h2>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+        </Typography>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-8 p-4">
           {TESTIMONIES.map((testimony) => (
             <TestimonyCard key={testimony.author} details={testimony} />
           ))}
@@ -29,26 +30,37 @@ type CardProps = {
 };
 const TestimonyCard = ({ details }: { details: CardProps }) => {
   return (
-    <Card className="flex bg-secondary relative overflow-hidden cursor-pointer min-w-[15rem] md:min-w-[20rem] min-h-[24rem] group flex-col z-[1] items-start justify-start px-6 py-8 shadow-lg">
+    <Card className="flex bg-card relative overflow-hidden cursor-pointer group flex-col z-[1] items-start justify-start p-4 shadow-md">
       <div
         aria-hidden
         className="absolute h-[20rem] transition-all -bottom-72 -left-40 bg-transparent group:hover:bottom-0  group-hover:left-0 rounded-tr-3xl w-full group-hover:bg-primary/10 blur-xl -z-[1]"
       ></div>
-      <blockquote className="text-lg first-letter:text-primary first-letter:text-xl font-medium leading-relaxed mb-20 justify-start text-left">
-        {details.review}
-      </blockquote>
-      <Avatar className="mt-auto">
-        <AvatarImage
-          alt={details.author}
-          src={details.profilePic}
-          className="object-cover"
-        />
-        <AvatarFallback>{details.fallback}</AvatarFallback>
-      </Avatar>
-      <h3 className="font-semibold mt-2">{details.author}</h3>
-      <p className="text-sm text-gray-500">
-        {details.role + ", " + details.brand}
-      </p>
+      <CardContent className="">
+        <Typography
+          variant={"p"}
+          className="prose text-start text-pretty max-h-[18rem]"
+        >
+          {details.review}
+        </Typography>
+      </CardContent>
+      <CardFooter className="mt-auto">
+        <Avatar className="mr-4">
+          <AvatarImage
+            alt={details.author}
+            src={details.profilePic}
+            className="object-cover"
+          />
+          <AvatarFallback>{details.fallback}</AvatarFallback>
+        </Avatar>
+        <div className="flex flex-col justify-start items-start mt-2 gap-2">
+          <Typography className="text-card-foreground">
+            {details.author}
+          </Typography>
+          <Typography variant={"mutedText"}>
+            {details.role + ", " + details.brand}
+          </Typography>
+        </div>
+      </CardFooter>
     </Card>
   );
 };
