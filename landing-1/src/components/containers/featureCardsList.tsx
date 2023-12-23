51b9c0d5f79420df6d76ca367a0ae3abb61b82cb
Sreejith K <srejitk@gmail.com>
2023-12-24 02:03:58 +0530

    Chore : Added Section and scroll-snap

--- landing-1/src/components/containers/featureCardsList.tsx
+++ landing-1/src/components/containers/featureCardsList.tsx
@@ -2,9 +2,13 @@ import React from "react";
 import FeatureCard from "./featureCard";
 import { SHOWCASE_FEATURES } from "@/lib/enums";
 const FeatureCardsList = () => {
-  return SHOWCASE_FEATURES.map((feature) => (
-    <FeatureCard key={feature.title} feature={feature} />
-  ));
+  return (
+    <section className="snap-start">
+      {SHOWCASE_FEATURES.map((feature) => (
+        <FeatureCard key={feature.title} feature={feature} />
+      ))}
+    </section>
+  );
 };
 
 export default FeatureCardsList;
