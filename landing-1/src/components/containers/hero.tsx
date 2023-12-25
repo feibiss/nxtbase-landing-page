d9ae9835145e129ab056549faa635dc98537492e
Sreejith K <srejitk@gmail.com>
2023-12-26 00:45:22 +0530

    Style : Updated styling of Hero

--- landing-1/src/components/containers/hero.tsx
+++ landing-1/src/components/containers/hero.tsx
@@ -16,49 +16,55 @@ export default function Hero() {
     <section className="text-center h-[calc(100vh-var(--navbar-height))] snap-start scroll-mt-[6rem] gap-2 flex flex-col items-center justify-center">
       <div className="absolute inset-0 -z-10 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:150px_180px] [mask-image:radial-gradient(ellipse_30%_40%_at_50%_55%,#000_70%,transparent_110%)]"></div>
       {/* ? Pill */}
-      <div className="w-fit flex items-center justify-center rounded-full gap-2 border-2 border-primary/20">
+      <div className="w-fit flex items-center justify-center rounded-full gap-2 border-2 border-amber-400/20 dark:border-amber-300/50">
         <Link
           href="#"
           className={clsx(
-            buttonVariants({ variant: "ghost", size: "sm" }),
-            " rounded-full flex justify-start items-center h-fit w-full bg-sky-50 dark:bg-muted"
+            " rounded-full flex justify-start items-center h-6 gap-2 p-1 bg-amber-50 dark:bg-amber-900"
           )}
         >
           <p
             className={clsx(
-              buttonVariants({ variant: "default" }),
-              "text-xs rounded-full text-primary-foreground "
+              "text-xs rounded-full bg-amber-400 w-fit h-full px-2 text-primary-foreground "
             )}
           >
             New
           </p>
-          <p className="text-primary text-sm font-semibold ">
+          <p className="text-amber-600 text-sm font-semibold ">
             Latest update details hook here.
           </p>
         </Link>
       </div>
-      <Typography variant="h1" className="max-w-5xl">
+      <Typography variant="h1" className="md:max-w-4xl lg:max-w-5xl max-w-3xl">
         A fancy looking hero text to catch your attention
       </Typography>
-      <Typography variant="p" className="max-w-xl text-muted-foreground">
+      <Typography
+        variant="p"
+        className="md:max-w-xl max-w-sm text-muted-foreground"
+      >
         Great, now that we have your attention, we will actually talk about how
         we help you do the above. Mostly a brief description which explains our
         service to convince you to keep scrolling down.
       </Typography>
 
-      <Button variant={"default"} size={"lg"} className="my-4">
-        <div className="flex w-full justify-between items-center">
+      <Button variant={"default"} className="my-4 w-fit px-2 py-0.5">
+        <div className="flex w-fit justify-between items-center gap-2">
           <p className="">Get started for free</p>
-          <div className="bg-white rounded-full h-8 w-8 text-primary flex justify-center items-center">
+          <div className=" bg-primary-foreground/80 dark:bg-primary-foreground/80 rounded-full h-6 w-6 text-primary flex justify-center items-center">
             <ArrowRightIcon className="w-4 h-4" />
           </div>
         </div>
       </Button>
       <div className="flex justify-center space-x-2">
-        <CheckCircledIcon className="w-6 h-6 text-green-500" />
-        <span className="text-muted-foreground">No credit card required</span>
-        <CheckCircledIcon className="w-6 h-6 text-green-500" />
-        <span className="text-muted-foreground">7-day free trial</span>
+        <>
+          <CheckCircledIcon className="w-6 h-6 text-green-500" />
+          <span className="text-muted-foreground">No credit card required</span>
+        </>
+        <>
+          {" "}
+          <CheckCircledIcon className="w-6 h-6 text-green-500" />
+          <span className="text-muted-foreground">7-day free trial</span>
+        </>
       </div>
       <div className="flex justify-center -space-x-2 my-2">
         {AVATARS.map(({ src, id }) => {
