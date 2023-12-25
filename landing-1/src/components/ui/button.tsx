5f8d37d68ac1eb83db36f49a13eee406f6b4048c
Sreejith K <srejitk@gmail.com>
2023-12-26 02:35:42 +0530

    Style : Updated button styles & padding

--- landing-1/src/components/ui/button.tsx
+++ landing-1/src/components/ui/button.tsx
@@ -4,12 +4,12 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 
 const buttonVariants = cva(
-  "inline-flex items-center w-fit p-1 justify-center overflow-hidden whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 shadow-[0px_1px_1px_1px_#ffffff] dark:shadow-[0px_1px_1px_1px_#10000000]",
+  "inline-flex items-center w-fit justify-center overflow-hidden whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 shadow-[0px_1px_1px_1px_#ffffff] dark:shadow-[0px_1px_1px_1px_#10000000]",
   {
     variants: {
       variant: {
         default:
-          " bg-gradient-to-b from-primary to-indigo-600 hover:to-indigo-700 text-primary-foreground hover:bg-primary/90",
+          " bg-gradient-to-b px-2 py-0.5 from-primary to-indigo-600 hover:to-indigo-700 text-white hover:bg-primary/90",
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
