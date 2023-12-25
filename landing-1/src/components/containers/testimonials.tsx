fd952f6886a52a574df9c54d941b60cf189c715b
Sreejith K <srejitk@gmail.com>
2023-12-26 01:30:44 +0530

    Feat : Added md responsiveness

--- landing-1/src/components/containers/testimonials.tsx
+++ landing-1/src/components/containers/testimonials.tsx
@@ -5,12 +5,12 @@ import { Typography } from "../ui/typography";
 
 export default function Testimonials() {
   return (
-    <section className="snap-start w-full max-w-6xl">
+    <section className="snap-start w-full h-fit max-w-6xl my-32">
       <div className="mx-auto text-center bg-background">
-        <Typography variant={"h2"} className="prose text-pretty">
+        <Typography variant={"h2"} className="prose text-pretty mb-8">
           What Our Users Are Saying
         </Typography>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-8 p-4">
+        <div className="grid grid-cols-1 md:grid-cols-6 lg:grid-cols-3 lg:grid-rows-1 md:grid-rows-2  gap-8 p-4">
           {TESTIMONIES.map((testimony) => (
             <TestimonyCard key={testimony.author} details={testimony} />
           ))}
@@ -30,7 +30,7 @@ type CardProps = {
 };
 const TestimonyCard = ({ details }: { details: CardProps }) => {
   return (
-    <Card className="flex bg-card relative overflow-hidden cursor-pointer group flex-col z-[1] items-start justify-start p-4 shadow-md">
+    <Card className="flex lg:col-span-1 md:col-span-3 md:last:col-start-2 lg:last:col-start-3 lg:last:col-span-1 md:last:col-span-4 relative overflow-hidden cursor-pointer group flex-col z-[1] items-start justify-start p-4 shadow-md">
       <div
         aria-hidden
         className="absolute h-[20rem] transition-all -bottom-72 -left-40 bg-transparent group:hover:bottom-0  group-hover:left-0 rounded-tr-3xl w-full group-hover:bg-primary/10 blur-xl -z-[1]"
