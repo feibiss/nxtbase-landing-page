6140c42d3c3a479ef503f68ecd2cb0574fc7f061
Sreejith K <srejitk@gmail.com>
2023-12-26 14:57:50 +0530

    Chore : Updated titles

--- landing-1/src/lib/enums.ts
+++ landing-1/src/lib/enums.ts
@@ -56,7 +56,7 @@ export const AVATARS = [
 
 export const SHOWCASE_FEATURES = [
   {
-    title: "Built in user authentication system",
+    title: "Feature One",
     subtitle: "User Authentication",
     navigateTo: "#",
     desc: "Nextbase supports a wide range of authentication providers such as Google, GitHub, Twitter, Facebook, Apple, Discord and many more. It also provides the flexibility for users to sign in with their email and password or even just with their email.",
@@ -69,7 +69,7 @@ export const SHOWCASE_FEATURES = [
       "https://res.cloudinary.com/dkqrmlxlg/video/upload/v1702921481/usenextbase/tinywow_signup_43386486_t9iuny.webm",
   },
   {
-    title: "Built in Admin Panel",
+    title: "Feature Two",
     subtitle: "Admin Panel",
     navigateTo: "#",
     desc: "Nextbase comes with a built-in admin panel that allows you to have a secret area within your app where you can manage users, teams, organizations and more. It provides a comprehensive and intuitive interface for managing all aspects of your application, from users to projects and organizations.",
@@ -81,7 +81,7 @@ export const SHOWCASE_FEATURES = [
       "https://res.cloudinary.com/dkqrmlxlg/video/upload/v1702915076/usenextbase/tinywow_admin-panel-gif_43379830_tltcqi.webm",
   },
   {
-    title: "Debug with User impersonation",
+    title: "Feature Three",
     subtitle: "User Impersonation",
     navigateTo: "#",
     features: [
@@ -95,7 +95,7 @@ export const SHOWCASE_FEATURES = [
       "https://res.cloudinary.com/dkqrmlxlg/video/upload/v1702921499/usenextbase/tinywow_user-imp_43386586_xpo6pr.webm",
   },
   {
-    title: "Server Components Integration",
+    title: "Feature Four",
     subtitle: "Incredible performance",
     navigateTo: "#",
     features: [
@@ -108,7 +108,7 @@ export const SHOWCASE_FEATURES = [
       "https://res.cloudinary.com/dkqrmlxlg/video/upload/v1702916928/usenextbase/tinywow_layouts_43381898_qpovdj.webm",
   },
   {
-    title: "Stripe Payments",
+    title: "Feature Five",
     subtitle: "Simplified Payment flow",
     navigateTo: "#",
     features: [
