105c06e57dbc5b90988eadb263fb87f68fec1d87
Sreejith K <srejitk@gmail.com>
2023-12-26 14:50:39 +0530

    Fix : Updated banner image

--- landing-1/src/components/containers/banner.tsx
+++ landing-1/src/components/containers/banner.tsx
@@ -18,7 +18,7 @@ const Banner = () => {
       ref={targetRef}
       className="h-[50dvh] md:h-[75vh]  snap-start rounded-xl relative flex justify-center md:items-start lg:pt-10 lg:mt-16"
     >
-      <div className="bg-gradient-to-b z-10 inset-0 from-primary via-blue-400 bottom-0 flex mt-auto  to-transparent h-full w=full rounded-t-[5rem]"></div>
+      <div className="bg-gradient-to-b z-0 from-blue-400 via-indigo-100 bottom-0 flex mt-auto  to-transparent md:h-[70vh] lg:h-[60vh] md:w-[95vw] lg:w-[80vw] rounded-t-[5rem]"></div>
       <motion.div
         style={{
           scale,
@@ -29,20 +29,20 @@ const Banner = () => {
           top: "50%",
           left: "50%",
         }}
-        className=" w-[768px] lg:w-[1024px] absolute antialiased hidden md:block [perspective:1200px] z-10 shadow-[0_24px_48px_-12px_#0f17282e] md:h-[578px] lg:h-[640px] mx-auto rounded-lg overflow-hidden  "
+        className=" w-[768px] border-[1px] border-indigo-900/20 lg:w-[1024px] absolute antialiased hidden md:block [perspective:1200px] z-10 shadow-lg md:h-[578px] lg:h-[640px] mx-auto rounded-lg overflow-hidden  "
       >
         <Image
           alt="Hero image"
           fill
           className="hidden md:block"
-          src="https://framerusercontent.com/images/zDvCdrgFgrnqx6RM3w6Vm6nzTc.png?scale-down-to=1024"
+          src="https://res.cloudinary.com/dkqrmlxlg/image/upload/v1703582349/admin-dashboard_pzonvt.png"
         />
       </motion.div>
       <div className="max-w-sm md:hidden w-[80vw] top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 absolute antialiased z-[2] shadow-[0_24px_48px_-12px_#0f17282e] h-[55vw] rounded-lg overflow-hidden  ">
         <Image
           alt="Hero image"
           fill
-          src="https://framerusercontent.com/images/zDvCdrgFgrnqx6RM3w6Vm6nzTc.png?scale-down-to=1024"
+          src="https://res.cloudinary.com/dkqrmlxlg/image/upload/v1703582349/admin-dashboard_pzonvt.png"
         />
       </div>
     </section>
