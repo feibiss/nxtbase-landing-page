1a9df42ae25a9f7c9c897818288b24be53a58c28
Sreejith K <srejitk@gmail.com>
2023-12-27 23:37:15 +0530

    Chore : Removed borders

--- landing-1/src/components/containers/featureCardsList.tsx
+++ landing-1/src/components/containers/featureCardsList.tsx
@@ -3,7 +3,7 @@ import FeatureCard from "./featureCard";
 import { SHOWCASE_FEATURES } from "@/lib/enums";
 const FeatureCardsList = () => {
   return (
-    <section className="px-4 w-full h-full border-4">
+    <section className="px-4 w-full h-full">
       {SHOWCASE_FEATURES.map((feature) => (
         <FeatureCard key={feature.title} feature={feature} />
       ))}
