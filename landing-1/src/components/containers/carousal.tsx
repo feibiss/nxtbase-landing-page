a76f78abf8b800bdf0901b453fdecb6af26bfda0
Sreejith K <srejitk@gmail.com>
2023-12-27 23:37:06 +0530

    Style : Updated Header styles

--- landing-1/src/components/containers/carousal.tsx
+++ landing-1/src/components/containers/carousal.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import FeatureCarousel from "./featureCarousal";
+import { Typography } from "../ui/typography";
 
 const Carousal = () => {
   return (
     <>
-      <h2 className="text-2xl text-foreground flex justify-center items-center text-center pb-4 font-bold tracking-tighter sm:text-4xl md:text-3xl mb-10">
+      <Typography
+        variant={"h4"}
+        className="flex justify-center items-center text-center pb-4 font-bold tracking-tighter mb-8"
+      >
         Everything else you need
-      </h2>
+      </Typography>
       <FeatureCarousel />
     </>
   );
