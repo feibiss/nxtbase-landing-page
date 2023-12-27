b321f6098c3b36069ab4e624fc0288c5b70656a5
Sreejith K <srejitk@gmail.com>
2023-12-27 23:37:40 +0530

    Style : Updated h4 styles

--- landing-1/src/components/ui/typography.tsx
+++ landing-1/src/components/ui/typography.tsx
@@ -9,7 +9,7 @@ const typographyVariants = cva("text-foreground", {
       h1: "scroll-m-20 text-5xl font-semibold tracking-tight lg:text-7xl",
       h2: "scroll-m-20 pb-2 text-4xl font-semibold tracking-tight first:mt-0",
       h3: "scroll-m-20 text-3xl font-semibold tracking-tight",
-      h4: "scroll-m-20 text-2xl font-semibold tracking-tight",
+      h4: "scroll-m-20 text-2xl font-semibold sm:text-4xl md:text-3xl tracking-tight",
       h5: "scroll-m-20 text-lg font-semibold tracking-tight",
       h6: "scroll-m-20 text-base font-semibold tracking-tight",
       p: "leading-7 [&:not(:first-child)]:mt-6",
