e3450fa7a39e0073142ba564b9910054ce4a483d
Sreejith K <srejitk@gmail.com>
2023-12-27 23:54:38 +0530

    Chore : Wrapped content in section

--- landing-1/src/components/containers/carousal.tsx
+++ landing-1/src/components/containers/carousal.tsx
@@ -4,7 +4,7 @@ import { Typography } from "../ui/typography";
 
 const Carousal = () => {
   return (
-    <>
+    <section className="my-8">
       <Typography
         variant={"h4"}
         className="flex justify-center items-center text-center pb-4 font-bold tracking-tighter mb-8"
@@ -12,7 +12,7 @@ const Carousal = () => {
         Everything else you need
       </Typography>
       <FeatureCarousel />
-    </>
+    </section>
   );
 };
 
