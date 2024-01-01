14caa943d07ecb2a7cac7428906d03ec1f339c80
Sreejith K <kumar.edwardcullen.sreejith80@gmail.com>
2024-01-02 01:05:50 +0530

    Style : Updated max width

--- landing-1/src/components/containers/container.tsx
+++ landing-1/src/components/containers/container.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
-const Container = (children : React.ReactNode) => {
+const Container = ({ children }: { children: React.ReactNode }) => {
   return (
-    <section className='mx-auto relative lg:px-32 md:px-16 px-8 '>
+    <section className="mx-auto grid grid-cols-12 max-w-7xl relative lg:px-32 md:px-16 px-8 ">
       {children}
     </section>
-  )
-}
+  );
+};
 
-export default Container
+export default Container;
