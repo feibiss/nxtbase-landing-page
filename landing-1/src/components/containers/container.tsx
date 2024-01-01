778cfb4713a9df26ad2322804004ab178117fec1
Sreejith K <kumar.edwardcullen.sreejith80@gmail.com>
2024-01-01 23:51:57 +0530

    Style : Added Margin

--- landing-1/src/components/containers/container.tsx
+++ landing-1/src/components/containers/container.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Container = (children : React.ReactNode) => {
   return (
-    <section className=' lg:px-32 md:px-16 px-8 '>
+    <section className='mx-auto relative lg:px-32 md:px-16 px-8 '>
       {children}
     </section>
   )
