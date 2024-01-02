41121c192a59235b2cd60f3d93b06d4a16a49aa8
Sreejith K <kumar.edwardcullen.sreejith80@gmail.com>
2024-01-03 00:17:37 +0530

    Fix : Smaller grid sizes

--- landing-1/src/components/containers/container.tsx
+++ landing-1/src/components/containers/container.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Container = ({ children }: { children: React.ReactNode }) => {
   return (
-    <section className="mx-auto grid grid-cols-12 max-w-7xl relative lg:px-32 md:px-16 px-8 ">
+    <section className="mx-auto w-full h-full grid grid-cols-12 max-w-7xl relative lg:px-32 md:px-16 px-8 ">
       {children}
     </section>
   );
