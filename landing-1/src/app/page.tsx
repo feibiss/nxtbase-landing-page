10fd55bc1de3590509cc3ffe434460e7565db23d
Sreejith K <kumar.edwardcullen.sreejith80@gmail.com>
2024-01-03 01:38:05 +0530

    Chore : Added spacer after banner

--- landing-1/src/app/page.tsx
+++ landing-1/src/app/page.tsx
@@ -7,8 +7,10 @@ import Spacer from "@/components/containers/spacer";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-between">
+    <main className="flex flex-col items-center justify-between relative">
+      <div className="absolute inset-0 -z-10 h-full w-full dark:bg-primary-foreground bg-primary-foreground bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] dark:bg-[radial-gradient(#4b5563_1px,transparent_1px)] [background-size:32px_32px]"></div>
       <Hero />
+      <Spacer type="md" />
       <Banner />
       <Testimonials />
       <Spacer type="lg" />
