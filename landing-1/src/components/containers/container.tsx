34f940b25c0b41915225d6c323ecc1eda66be55c
Sreejith K <kumar.edwardcullen.sreejith80@gmail.com>
2024-01-03 01:38:38 +0530

    Feat : Added support for custom classnames

--- landing-1/src/components/containers/container.tsx
+++ landing-1/src/components/containers/container.tsx
@@ -1,8 +1,20 @@
+import { cn } from "@/lib/utils";
 import React from "react";
 
-const Container = ({ children }: { children: React.ReactNode }) => {
+const Container = ({
+  children,
+  classNames,
+}: {
+  children: React.ReactNode;
+  classNames?: string;
+}) => {
   return (
-    <section className="mx-auto w-full h-full grid grid-cols-12 max-w-7xl relative lg:px-32 md:px-16 px-8 ">
+    <section
+      className={cn(
+        "mx-auto w-full h-full grid grid-cols-12 max-w-7xl relative lg:px-32 md:px-16 px-8 ",
+        classNames
+      )}
+    >
       {children}
     </section>
   );
