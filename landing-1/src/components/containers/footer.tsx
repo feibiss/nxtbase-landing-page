526ab009265ef22630320cf35f26d2ab26878b2c
Sreejith K <kumar.edwardcullen.sreejith80@gmail.com>
2024-01-05 01:18:32 +0530

    Style : Updated footer design

--- landing-1/src/components/containers/footer.tsx
+++ landing-1/src/components/containers/footer.tsx
@@ -8,15 +8,15 @@ import { FOOTER_LINKS, FooterCategory } from "@/lib/enums";
 export default function Footer() {
   return (
     <footer className=" relative bg-gray-900 px-8 md:pt-8 lg:h-[calc(100dvh-var(--navbar-height))] lg:pt-8">
-      <div className="grid grid-cols-1 gap-8 lg:grid-cols-3 lg:gap-40">
-        <div className="order-2 col-span-1 grid w-full grid-cols-3 gap-4 pt-12 md:order-2 md:mx-auto md:mt-8 md:grid-cols-5 md:gap-8 lg:order-1 lg:col-span-2  lg:mt-0">
+      <div className="grid grid-cols-1 gap-8 lg:grid-cols-3 lg:gap-10">
+        <div className="order-2 col-span-1 flex flex-wrap justify-between lg:justify-normal items-center w-full  gap-4 pt-12 md:order-2 md:mx-auto md:mt-8 md:gap-8 lg:col-span-2  lg:mt-0">
           {FOOTER_LINKS.map((details) => {
             return <FooterCategory key={details.id} footerDetails={details} />;
           })}
         </div>
-        <div className="col-span-1 mx-auto w-full space-y-4 pt-16 md:px-10 md:pt-8 lg:pt-12">
+        <div className="col-span-1 lg:order-2 order-1 mx-auto w-full space-y-4 pt-16 md:px-10 md:pt-8 lg:pt-12">
           <div className="space-y-2 text-start">
-            <Typography variant={"h4"} className=" text-gray-200">
+            <Typography variant={"h5"} className=" text-gray-200">
               Subscribe to our Newsletter
             </Typography>
             <Typography className=" text-gray-400">
@@ -62,7 +62,7 @@ const FooterCategory = ({
   footerDetails: FooterCategory;
 }) => {
   return (
-    <div className="mb-8 flex flex-col md:items-baseline">
+    <div className="mb-8 flex lg:w-1/4 flex-col md:items-baseline">
       <Typography
         variant={"h5"}
         className="mb-4 text-lg font-bold text-gray-100"
