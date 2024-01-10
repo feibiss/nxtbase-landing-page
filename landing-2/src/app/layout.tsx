05a3382285c9dd138b1c79ecff81000956cf5128
Sreejith K <kumar.edwardcullen.sreejith80@gmail.com>
2024-01-11 01:40:52 +0530

    Feat : Added Background

--- landing-2/src/app/layout.tsx
+++ landing-2/src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/containers/header";
 import Footer from "@/components/containers/footer";
+import BackgroundBanner from "@/components/ui/bg";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <BackgroundBanner />
         <Header />
         {children}
         <Footer />
