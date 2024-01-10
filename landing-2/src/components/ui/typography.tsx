ed53bb95c5e2e2740bee98e8f900de827d895849
Sreejith K <kumar.edwardcullen.sreejith80@gmail.com>
2024-01-10 13:47:43 +0530

    STyle : Updated typography styles

--- landing-2/src/components/ui/typography.tsx
+++ landing-2/src/components/ui/typography.tsx
@@ -6,11 +6,11 @@ import { Slot } from "@radix-ui/react-slot";
 const typographyVariants = cva("text-foreground", {
   variants: {
     variant: {
-      h1: "scroll-m-20 text-5xl font-semibold tracking-tight lg:text-7xl",
-      h2: "scroll-m-20 pb-2 text-4xl font-semibold tracking-tight first:mt-0",
-      h3: "scroll-m-20 text-3xl font-semibold tracking-tight",
-      h4: "scroll-m-20 text-2xl font-semibold sm:text-4xl md:text-3xl tracking-tight",
-      h5: "scroll-m-20 text-lg font-semibold tracking-tight",
+      h1: "scroll-m-20 text-6xl font-bold tracking-tighter lg:text-7xl",
+      h2: "scroll-m-20 pb-2 text-5xl font-bold tracking-tight first:mt-0",
+      h3: "scroll-m-20 text-4xl font-semibold tracking-tight",
+      h4: "scroll-m-20 text-3xl font-semibold sm:text-4xl md:text-3xl tracking-tight",
+      h5: "scroll-m-20 text-2xl font-semibold tracking-tight",
       h6: "scroll-m-20 text-base font-semibold tracking-tight",
       p: "leading-7 [&:not(:first-child)]:mt-6",
       blockquote: "mt-6 border-l-2 pl-6 italic",
