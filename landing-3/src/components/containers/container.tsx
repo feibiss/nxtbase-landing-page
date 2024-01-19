2a8a7c15a005e61b46b55b5e4c595bd3ba5214e1
Sreejith K <kumar.edwardcullen.sreejith80@gmail.com>
2024-01-19 14:00:14 +0530

    STyle : Updated container width styling

--- landing-3/src/components/containers/container.tsx
+++ landing-3/src/components/containers/container.tsx
@@ -11,7 +11,7 @@ const Container = ({
   return (
     <section
       className={cn(
-        "relative mx-auto grid h-fit w-full lg:max-w-7xl md:max-w-2xl grid-cols-12 ",
+        "relative mx-auto grid overflow-hidden xl:max-w-none flex-col align-center justify-center py-10 px-5 lg:px-8 2xl:px-20 h-fit w-full lg:max-w-7xl md:max-w-2xl grid-cols-12 ",
         classNames
       )}
     >
