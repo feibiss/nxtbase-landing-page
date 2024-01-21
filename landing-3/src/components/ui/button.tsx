e439b8fa38259ef431a6b61b28ed67debb312c8a
Sreejith K <kumar.edwardcullen.sreejith80@gmail.com>
2024-01-21 23:59:11 +0530

    Style : Styles updated

--- landing-3/src/components/ui/button.tsx
+++ landing-3/src/components/ui/button.tsx
@@ -4,7 +4,7 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 
 const buttonVariants = cva(
-  "inline-flex p-3.5 lg:p-4 px-10 lg:px-16 text-md lg:text-lg font-bold items-center w-fit tracking-tight cta-btn cursor-pointer hover:scale-105 duration-300 ease-out justify-center overflow-hidden whitespace-nowrap ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-white text-primary shadow-[inset_0_-4px_0_0_rgba(0,0,0,.25)] h-fit min-h-10 rounded-sm mb-4",
+  "inline-flex max-w-5xl p-3.5 lg:p-4 px-10 lg:px-16 text-md lg:text-lg font-bold items-center w-fit tracking-tight cta-btn cursor-pointer hover:scale-105 duration-300 ease-out justify-center overflow-hidden whitespace-nowrap ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-white text-primary shadow-[inset_0_-4px_0_0_rgba(0,0,0,.25)] h-fit min-h-10 rounded-sm mb-4",
   {
     variants: {
       variant: {
@@ -14,14 +14,14 @@ const buttonVariants = cva(
         outline:
           "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
         secondary:
-          "bg-secondary text-secondary-foreground hover:bg-secondary/80",
+          "bg-gray-800 mt-8 rounded-xl border p-4 outline outline-white text-white w-full hover:bg-white hover:text-primary",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
         default: "h-fit min-h-10",
         sm: "h-9",
-        lg: "h-11 px-8 py-6",
+        lg: "h-[4rem] md:h-[7rem] lg:px-16 text-2xl font-bold",
         icon: "h-10 w-10",
       },
       rounded: {
