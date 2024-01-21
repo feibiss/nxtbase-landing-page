722fa8fdc6d89910a614e77e89395e805a33008d
Sreejith K <kumar.edwardcullen.sreejith80@gmail.com>
2024-01-21 23:51:28 +0530

    Style : Updated contact card styles

--- landing-3/src/components/containers/contact.tsx
+++ landing-3/src/components/containers/contact.tsx
@@ -8,18 +8,11 @@ export default function ContactCard() {
     <Container classNames="mt-20 border-t border-gray-200 dark:border-gray-800 bg-card/20">
       <div className="col-span-12 relative mx-auto h-fullflex flex-col justify-start items-center overflow-x-hidden w-full overflow-hidden">
         <div className=" bottom-0 flex h-[30rem] w-full flex-col items-center justify-center  ">
-          <Typography variant={"h2"}>
-            Grow with{" "}
-            <span className="bg-gradient-to-r from-red-600 to-amber-600 bg-clip-text text-transparent">
-              Nextbase.
-            </span>
+          <Typography variant={"h1"} className="text-center mx-auto">
+            Build your next SAAS project with a modern stack and save 4+ months
+            of development time ~ $18000
           </Typography>
-          <Typography className="prose my-1 text-pretty px-4 text-center text-gray-800 md:px-0 dark:text-gray-300">
-            Build your next SAAS project with a modern stack of Next.js 14+,
-            Supabase, Stripe and Typescript, and save 4+ months of development
-            time ~ $18000
-          </Typography>
-          <Button className="my-4 mt-8">
+          <Button className="my-4 bg-black mt-8 rounded-xl border p-4 h-[4rem] md:h-[7rem] lg:px-16 text-2xl font-bold text-white cta-btn cursor-pointer hover:scale-105 transition duration-300 ease-out w-full hover:bg-white hover:text-primary">
             <div className="flex w-fit items-center justify-between gap-2">
               <p className="">Get started for free</p>
 
