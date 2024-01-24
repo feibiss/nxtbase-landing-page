85f3cfd47cad3dee82dea2c04e54affe3c3eb3a1
Sreejith K <kumar.edwardcullen.sreejith80@gmail.com>
2024-01-25 00:31:16 +0530

    Style : updated button styles

--- landing-3/src/components/ui/button.tsx
+++ landing-3/src/components/ui/button.tsx
@@ -4,15 +4,14 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 
 const buttonVariants = cva(
-  "inline-flex max-w-5xl p-3.5 lg:p-4 px-10 lg:px-16 text-md lg:text-lg font-bold items-center w-fit tracking-tight cta-btn cursor-pointer hover:scale-105 duration-300 ease-out justify-center overflow-hidden whitespace-nowrap ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-white text-primary shadow-[inset_0_-4px_0_0_rgba(0,0,0,.25)] h-fit min-h-10 rounded-sm mb-4",
+  "inline-flex text-md p-3.5  px-10  font-bold items-center w-fit tracking-tight cursor-pointer hover:scale-105 duration-300 ease-out justify-center overflow-hidden whitespace-nowrap ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 shadow-[inset_0_-4px_0_0_rgba(0,0,0,.25)] h-fit min-h-10 mb-4",
   {
     variants: {
       variant: {
-        default: "",
+        default: "bg-white text-primary",
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
-        outline:
-          "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
+        outline: "text-white hover:bg-white hover:text-primary font-semibold",
         secondary:
           "bg-gray-800 mt-8 rounded-xl border p-4 outline outline-white text-white w-full hover:bg-white hover:text-primary",
         ghost: "hover:bg-accent hover:text-accent-foreground",
@@ -21,13 +20,13 @@ const buttonVariants = cva(
       size: {
         default: "h-fit min-h-10",
         sm: "h-9",
-        lg: "h-[4rem] md:h-[7rem] lg:px-16 text-2xl font-bold",
+        lg: "max-w-5xl lg:py-4 lg:px-16 lg:text-lg h-[4rem] md:h-[7rem] text-2xl font-bold",
         icon: "h-10 w-10",
       },
       rounded: {
-        default: "rounded-sm",
+        default: "rounded-xl",
         sm: "rounded-md",
-        lg: "rounded-lg",
+        lg: "rounded-xl lg:px-16 lg:text-lg",
         full: "rounded-full",
       },
     },
