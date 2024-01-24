706c2e4b5a6f18ce84e250c909fd368a6758d89f
Sreejith K <kumar.edwardcullen.sreejith80@gmail.com>
2024-01-24 13:55:42 +0530

    Style : Updated styles + added assets

--- landing-3/src/components/containers/walkthrough.tsx
+++ landing-3/src/components/containers/walkthrough.tsx
@@ -26,14 +26,55 @@ ongoing support, we&apos;ve got you covered.`,
 const WalkThrough = () => {
   return (
     <Container classNames="bg-transparent">
-      <div className="col-span-12 flex flex-col items-center justify-center space-y-6 lg:space-y-12 bg-gradient-to-b from-[#1a1a1a] to-white/15 rounded-2xl p-10">
-        <p className={"text-sm font-base text-gray-400x [text-wrap:balance]"}>
+      <div className="col-span-12 max-w-7xl xl:max-w-[1400px] mt-20  lg:mt-[12rem] mx-auto flex flex-col items-center justify-center space-y-6 lg:space-y-12 bg-gradient-to-b from-[#1a1a1a] to-transparent rounded-2xl p-10">
+        <p
+          className={
+            "text-sm uppercase font-base text-gray-400x [text-wrap:balance]"
+          }
+        >
           Latest update details hook here.
         </p>
-        <Typography variant={"h1"}>Supafast, Supaeasy</Typography>
-        <div className="flex justify-between w-full">
+        <Typography variant={"h2"} className="max-w-7xl relative">
+          Supafast,{" "}
+          <span className="inline-flex">
+            Supaeasy
+            <Image
+              alt="Smile Scribble"
+              width={50}
+              height={50}
+              className="w-[30px] lg:w-[50px] hidden md:flex"
+              src="smile-scribble.svg"
+            />
+          </span>
+          <Image
+            alt="scribble"
+            loading="eager"
+            width="280"
+            height="50"
+            className="absolute -bottom-12 right-12 z-10 w-[100px] lg:w-[160px] xl:w-[280px]"
+            src="random-scribble.svg"
+          />
+        </Typography>
+        <div className="flex justify-between w-full pt-10">
           {STEPS?.map(({ id, title, content }) => (
-            <WalkThroughCard key={id} id={id} title={title} content={content} />
+            <>
+              <WalkThroughCard
+                key={id}
+                id={id}
+                title={title}
+                content={content}
+              />
+              {id < 3 && (
+                <Image
+                  alt="arrow-scribble"
+                  loading="eager"
+                  width="50"
+                  height="50"
+                  className="rotate-90 lg:rotate-0"
+                  src="arrow-scribble.svg"
+                />
+              )}
+            </>
           ))}
         </div>
         <div className="pt-10 flex justify-between items-center space-x-4">
