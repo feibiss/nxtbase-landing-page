415e8439b8bfdf6fc0868f6f65006217a21d049e
Sreejith K <kumar.edwardcullen.sreejith80@gmail.com>
2024-01-24 10:38:14 +0530

    Style : Added heart svg

--- landing-3/src/components/containers/testimonials.tsx
+++ landing-3/src/components/containers/testimonials.tsx
@@ -15,7 +15,19 @@ export default function Testimonials() {
       <div className="relative col-span-12 isolate bg-transparent pt-24 sm:pt-32">
         <div className="mx-auto max-w-8xl space-y-10">
           <div className="mx-auto text-center flex flex-col justify-start items-center gap-4">
-            <Typography variant={"h2"}>Loved by industry leaders</Typography>
+            <Typography variant={"h2"}>
+              Loved
+              <span className="inline-block pl-2 pr-2 xl:pl-5 xl:pr-5">
+                <Image
+                  alt="Heart Logo"
+                  src="heart-scribble.svg"
+                  width={70}
+                  height={50}
+                  className="w-[30px] lg:w-[40px] xl:w-[50px]"
+                />{" "}
+              </span>
+              by industry leaders
+            </Typography>
           </div>
           <ScrollingTestimonies />
           <div className="mx-auto pt-20 lg:pt-[12rem] flex flex-col items-center justify-center gap-8 text-sm leading-6 text-gray-900 dark:text-gray-300 xl:mx-0 xl:max-w-none">
