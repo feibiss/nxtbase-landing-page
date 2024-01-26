5dc5541c7f230cd157a2c80566d1b2c6e915f137
Sreejith K <kumar.edwardcullen.sreejith80@gmail.com>
2024-01-26 23:59:32 +0530

    Feat : Added Features card list

---
+++ landing-4/src/components/containers/featureCardsList.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import FeatureCard from "./featureCard";
+import { SHOWCASE_FEATURES } from "@/lib/enums";
+import Container from "./container";
+import { Typography } from "../ui/typography";
+const FeatureCardsList = () => {
+  return (
+    <Container classNames="bg-transparent overflow-visible">
+      <section className="col-span-12 row-auto grid h-full w-full gap-8 overflow-visible bg-transparent">
+        <div className="flex w-full justify-center items-center">
+          <Typography
+            variant={"h2"}
+            className=" text-card-foreground text-center mb-8 text-pretty font-bold tracking-tighter dark:text-white"
+          >
+            Features
+          </Typography>
+        </div>
+        {SHOWCASE_FEATURES.map((feature) => (
+          <FeatureCard key={feature.title} feature={feature} />
+        ))}
+      </section>
+    </Container>
+  );
+};
+
+export default FeatureCardsList;
