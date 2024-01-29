86f446a96ef9e29537b222f9b903761e0aff1721
Sreejith K <kumar.edwardcullen.sreejith80@gmail.com>
2024-01-30 00:42:25 +0530

    Style : Adjusted spacings

--- landing-4/src/components/containers/hero.tsx
+++ landing-4/src/components/containers/hero.tsx
@@ -8,19 +8,21 @@ import Brands from "./brands";
 export default function Hero() {
   return (
     <Container classNames="bg-primary">
-      <div className="col-span-12 mt-8 flex h-full snap-start flex-col items-center justify-center gap-1 text-center md:mt-[var(--navbar-height)] pt-20">
-        <Typography variant="h1">
-          A fancy looking hero text to catch your attention
-        </Typography>
-        <Typography
-          variant="p"
-          className="my-2 max-w-sm px-4 text-muted-foreground md:max-w-lg md:px-0"
-        >
-          Attention-grabbing alert! Brace yourself for an epic description of
-          how we rock at helping you out. Keep scrolling to be convinced.
-        </Typography>
+      <div className="col-span-12 flex h-full snap-start flex-col items-center justify-center space-y-8 py-4 text-center md:mt-[var(--navbar-height)] pt-20">
+        <div className="flex flex-col items-center justify-center space-y-2">
+          <Typography variant="h1">
+            A fancy looking hero text to catch your attention
+          </Typography>
+          <Typography
+            variant="p"
+            className="max-w-sm px-4 text-muted-foreground md:max-w-lg md:px-0"
+          >
+            Attention-grabbing alert! Brace yourself for an epic description of
+            how we rock at helping you out. Keep scrolling to be convinced.
+          </Typography>
+        </div>
 
-        <Button className="mb-4">
+        <Button className="">
           <div className="flex w-fit items-center justify-between gap-2">
             <p className="">Get started for free</p>
 
