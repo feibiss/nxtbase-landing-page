0a89c5146a5187126422168c8b8716c4894570e3
Sreejith K <kumar.edwardcullen.sreejith80@gmail.com>
2024-01-29 23:24:19 +0530

    Fix : inconsistent banner sizing

--- landing-4/src/components/containers/banner.tsx
+++ landing-4/src/components/containers/banner.tsx
@@ -9,20 +9,13 @@ const Banner = () => {
     target: targetRef,
     offset: ["start end", "end end"],
   });
-  const rotateX = useTransform(scrollYProgress, [0, 0.5, 1], [15, 10, 0]);
 
   return (
-    <div ref={targetRef} className="block h-[50dvh] w-full md:h-[75vh] mx-auto">
-      <motion.div
-        style={{
-          rotateX,
-          translateX: "-50%",
-          translateY: "0%",
-          top: "50%",
-          left: "50%",
-        }}
-        className=" absolute z-10 mx-auto hidden w-[768px] overflow-hidden rounded-lg border-[1px] border-indigo-900/20 antialiased shadow-lg [perspective:1200px] md:block md:h-[578px] lg:h-[640px] lg:w-[1024px]  "
-      >
+    <div
+      ref={targetRef}
+      className=" h-full w-full col-span-12 mx-auto flex justify-center items-center py-20"
+    >
+      <motion.div className=" z-10 mx-auto w-[768px] overflow-hidden rounded-lg border-[1px] border-indigo-900/20 antialiased shadow-lg [perspective:1200px] flex h-[578px] lg:h-[640px] lg:w-[1024px]  ">
         <Image
           alt="Hero image"
           fill
@@ -30,13 +23,6 @@ const Banner = () => {
           src="https://res.cloudinary.com/dkqrmlxlg/image/upload/v1703582349/admin-dashboard_pzonvt.png"
         />
       </motion.div>
-      <div className="absolute left-1/2 top-1/2 z-[2] h-[300px] w-[80vw] max-w-lg -translate-x-1/2 -translate-y-1/2 overflow-hidden rounded-lg antialiased shadow-[0_24px_48px_-12px_#0f17282e] md:hidden  ">
-        <Image
-          alt="Hero image"
-          fill
-          src="https://res.cloudinary.com/dkqrmlxlg/image/upload/v1703582349/admin-dashboard_pzonvt.png"
-        />
-      </div>
     </div>
   );
 };
