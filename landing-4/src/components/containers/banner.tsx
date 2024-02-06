c26426720c75b1d2c73f882924c1f06220131d00
Sreejith K <kumar.edwardcullen.sreejith80@gmail.com>
2024-02-07 00:41:19 +0530

    Chore : max width created

--- landing-4/src/components/containers/banner.tsx
+++ landing-4/src/components/containers/banner.tsx
@@ -15,11 +15,10 @@ const Banner = () => {
       ref={targetRef}
       className=" h-full w-full col-span-12 mx-auto flex justify-center items-center py-20"
     >
-      <motion.div className=" z-10 mx-auto w-[768px] overflow-hidden rounded-lg border-[1px] border-indigo-900/20 antialiased shadow-lg [perspective:1200px] flex h-[578px] lg:h-[640px] lg:w-[1024px]  ">
+      <motion.div className=" z-10 mx-auto max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl w-full overflow-hidden rounded-lg border-[1px] border-indigo-900/20 antialiased shadow-lg [perspective:1200px] flex h-[300px] md:h-[520px] lg:h-[640px] lg:w-[1024px]  ">
         <Image
           alt="Hero image"
           fill
-          className="hidden md:block"
           src="https://res.cloudinary.com/dkqrmlxlg/image/upload/v1703582349/admin-dashboard_pzonvt.png"
         />
       </motion.div>
