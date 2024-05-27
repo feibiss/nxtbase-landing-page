34bd9ef5f15a7c482da7ada9140b0e5b4e9d1b36
vaibhav18matere <vpm00718897@gmail.com>
2024-05-27 17:18:56 +0530

    feat: hero section ui fixed

--- landing-2/src/components/containers/hero.tsx
+++ landing-2/src/components/containers/hero.tsx
@@ -1,15 +1,10 @@
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "../ui/button";
-import {
-  ArrowRightIcon,
-  CheckCircledIcon,
-  StarFilledIcon,
-} from "@radix-ui/react-icons";
+import { ArrowRightIcon, CheckCircledIcon } from "@radix-ui/react-icons";
 import clsx from "clsx";
 import Link from "next/link";
-import { AVATARS } from "@/lib/enums";
 import { Typography } from "../ui/typography";
 import Container from "./container";
+import { CubeIcon } from "@radix-ui/react-icons";
 
 export default function Hero() {
   const STARS: number[] = new Array(5).fill(1);
@@ -21,79 +16,47 @@ export default function Hero() {
           <Link
             href="#"
             className={clsx(
-              "group mx-auto flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full border border-gray-200 bg-white px-7 py-2 shadow-[inset_10px_-50px_94px_0_rgb(199,199,199,0.1)] backdrop-blur transition-all hover:border-gray-300 hover:bg-white/50"
+              "group mx-auto flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full border border-gray-200 bg-[#3653141A] px-7 py-2 shadow-[inset_10px_-50px_94px_0_rgb(199,199,199,0.1)] backdrop-blur transition-all hover:border-gray-300 hover:bg-white/50"
             )}
           >
+            <CubeIcon className="color-black" />
             <p
               className={clsx(
-                "text-sm font-semibold text-gray-700 [text-wrap:balance]"
+                "text-sm font-semibold text-black [text-wrap:balance]"
               )}
             >
               Latest update details hook here.
             </p>
           </Link>
         </div>
-        <Typography variant="h1">
-          With great design comes &gt; &gt;{" "}
-          <span className=" bg-gradient-to-r from-amber-500 via-orange-600 to-yellow-500 bg-clip-text text-transparent">
-            hero text.
-          </span>{" "}
+        <Typography variant="h1" className="text-lime-900 text-center">
+          A fancy looking hero text to catch your attention
         </Typography>
         <Typography
           variant="p"
-          className="my-2 max-w-md px-4 text-muted-foreground md:max-w-lg md:px-0"
+          className="my-2 max-w-md px-4 text[#020817] md:max-w-lg md:px-0"
         >
           Great, now that we have your attention, we will actually talk about
-          how we help you do the above. Mostly a brief description which
-          explains our service to convince you to keep scrolling down.
+          how we help you
         </Typography>
-
-        <Button variant={"default"} className="my-4">
-          <div className="flex w-fit items-center justify-between gap-2">
-            <p className="">Get started for free</p>
-
-            <ArrowRightIcon className="h-4 w-4" />
-          </div>
-        </Button>
+        <div className="flex gap-4 my-6 px-6">
+          <Button variant={"outline"} className="">
+            Learn more
+          </Button>
+          <Button variant={"default"} className="">
+            <p>Get started for free</p>
+          </Button>
+        </div>
         <div className="flex flex-col items-center justify-center gap-4 space-x-2 md:flex-row">
-          <div className="flex w-fit items-center justify-start gap-2">
+          <div className="flex w-fit items-center justify-start gap-2 text[#020817]">
             {" "}
-            <CheckCircledIcon className="h-6 w-6 text-green-500" />
-            <span className="text-muted-foreground">7-day free trial</span>
+            <CheckCircledIcon className="h-6 w-6" />
+            <span>7-day free trial</span>
           </div>
           <div className="flex w-fit items-center justify-start gap-2">
-            <CheckCircledIcon className="h-6 w-6 text-green-500" />
-            <span className="text-muted-foreground">
-              No credit card required
-            </span>
+            <CheckCircledIcon className="h-6 w-6" />
+            <span>No credit card required</span>
           </div>
-        </div>
-        <div className="my-2 flex justify-center -space-x-2 ">
-          {AVATARS.map(({ src, id }) => {
-            return (
-              <Avatar key={id} className="border-4 border-muted">
-                <AvatarImage src={src} className="object-cover" />
-                <AvatarFallback>CN</AvatarFallback>
-              </Avatar>
-            );
-          })}
-        </div>
-        <div className="group mt-2 flex flex-col items-center justify-center md:flex-row">
-          <div className="mb-4 flex justify-start gap-1 md:mb-0">
-            {" "}
-            {STARS.map((_, idx) => {
-              return (
-                <StarFilledIcon
-                  key={_ + idx}
-                  className="h-5 w-5 text-yellow-400 hover:fill-yellow-400"
-                />
-              );
-            })}
-          </div>
-
-          <span className="ml-2 text-muted-foreground">
-            Loved by 570+ professionals
-          </span>
         </div>
       </div>
     </Container>
