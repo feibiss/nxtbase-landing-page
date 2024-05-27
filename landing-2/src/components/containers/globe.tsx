de411d7906d1eb90fd28e9e76a39aa233bfa039b
vaibhav18matere <vpm00718897@gmail.com>
2024-05-27 16:54:53 +0530

    fixed globe title text & styling

--- landing-2/src/components/containers/globe.tsx
+++ landing-2/src/components/containers/globe.tsx
@@ -7,11 +7,8 @@ const Globe = () => {
   return (
     <Container>
       <div className="mx-auto max-w-xl h-fit col-span-12 text-center flex flex-col justify-start items-center gap-4">
-        <Typography variant={"h2"}>
-          Built to scale{" "}
-          <span className="bg-gradient-to-r from-red-600 to-amber-600 bg-clip-text text-transparent">
-            with you.
-          </span>
+        <Typography variant={"h2"} className="text-lime-900">
+          Built to scale with you
         </Typography>
         <Typography variant={"p"} className="">
           Nextbase is built for scale. Whether you are building a small app or
