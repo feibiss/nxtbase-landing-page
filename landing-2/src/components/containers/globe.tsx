f213f79dad9ae7ac447c301c4f590b228a075159
vaibhav18matere <vpm00718897@gmail.com>
2024-05-27 20:55:14 +0530

    nextbase subtitle fixed

--- landing-2/src/components/containers/globe.tsx
+++ landing-2/src/components/containers/globe.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Container from "./container";
-import Image from "next/image";
 import { Typography } from "../ui/typography";
 
 const Globe = () => {
@@ -10,10 +9,9 @@ const Globe = () => {
         <Typography variant={"h2"} className="text-lime-900">
           Built to scale with you
         </Typography>
-        <Typography variant={"p"} className="">
-          Nextbase is built for scale. Whether you are building a small app or
-          an app to serve millions of users, Nextbase has all the tools you
-          need.
+        <Typography variant={"p"}>
+          Nextbase is built for scale. Whether you are building a small app or a
+          huge app, Nextbase has all the tools you need.
         </Typography>
       </div>
     </Container>
