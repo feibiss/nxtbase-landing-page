3b80e7606347ab86a0b84b651036f260808c1dd9
vaibhav18matere <vpm00718897@gmail.com>
2024-05-27 17:22:52 +0530

    contact card text and color ui fixed

--- landing-2/src/components/containers/contact.tsx
+++ landing-2/src/components/containers/contact.tsx
@@ -1,42 +1,40 @@
 import { Button } from "@/components/ui/button";
-import { ArrowRightIcon, CheckIcon } from "@radix-ui/react-icons";
+import { CheckCircledIcon } from "@radix-ui/react-icons";
 import { Typography } from "../ui/typography";
 import Container from "./container";
 
 export default function ContactCard() {
   return (
-    <Container classNames="mt-20 border-t border-gray-200 dark:border-gray-800 bg-card/20">
-      <div className="col-span-12 relative mx-auto h-fullflex flex-col justify-start items-center overflow-x-hidden w-full overflow-hidden">
+    <Container classNames="bg-card/20">
+      <div className="col-span-12 relative mx-auto h-full flex flex-col justify-start items-center overflow-x-hidden w-full overflow-hidden">
         <div className=" bottom-0 flex h-[30rem] w-full flex-col items-center justify-center  ">
-          <Typography variant={"h2"}>
-            Grow with{" "}
-            <span className="bg-gradient-to-r from-red-600 to-amber-600 bg-clip-text text-transparent">
-              Nextbase.
-            </span>
+          <Typography
+            variant={"h2"}
+            className="w-full text-center mx-auto text-lime-900"
+          >
+            Grow with Nextbase
           </Typography>
-          <Typography className="prose my-1 text-pretty px-4 text-center text-gray-800 md:px-0 dark:text-gray-300">
+          <Typography className="prose my-1 text-pretty px-4 text-center md:px-0 dark:text-gray-300">
             Build your next SAAS project with a modern stack of Next.js 14+,
             Supabase, Stripe and Typescript, and save 4+ months of development
             time ~ $18000
           </Typography>
-          <Button className="my-4 mt-8">
+          <Button className="my-4">
             <div className="flex w-fit items-center justify-between gap-2">
-              <p className="">Get started for free</p>
-
-              <ArrowRightIcon className="h-4 w-4" />
+              Get started for free
             </div>
           </Button>
           <div className="gap-4 flex flex-col md:flex-row justify-center items-center">
             <div className="flex items-center">
-              <CheckIcon className="mr-2 h-6 w-6 text-green-500" />
-              <span className="mr-8 font-semibold text-foreground">
-                No credit card required
+              <CheckCircledIcon className="mr-2 h-6 w-6" />
+              <span>
+                7-day free trial
               </span>
             </div>
             <div className="flex items-center">
-              <CheckIcon className="mr-2 h-6 w-6 text-green-500 " />
-              <span className="font-semibold text-foreground">
-                7-day free trial
+              <CheckCircledIcon className="mr-2 h-6 w-6" />
+              <span>
+                No credit card required
               </span>
             </div>
           </div>
