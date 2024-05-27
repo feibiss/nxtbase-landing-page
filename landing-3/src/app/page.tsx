e872422b70495e8e4042b4efe5056a94f0c9bfb5
vaibhav18matere <vpm00718897@gmail.com>
2024-05-28 02:00:10 +0530

    removed unnecessary components

--- landing-3/src/app/page.tsx
+++ landing-3/src/app/page.tsx
@@ -2,12 +2,7 @@ import Brands from "@/components/containers/brands";
 import Hero from "@/components/containers/hero";
 import { Showcase } from "@/components/containers/showcase";
 import Spacer from "@/components/ui/spacer";
-import FeatureList from "@/components/containers/featurelist";
-import Testimonials from "@/components/containers/testimonials";
 import ContactCard from "@/components/containers/contact";
-import WalkThrough from "@components/containers/walkthrough";
-import ShowcaseFeatures from "@components/containers/showcaseFeatures";
-import Questions from "@components/ui/question";
 import FAQ from "@components/ui/faq";
 import Pricing from "@components/containers/pricing";
 import FeaturedTestimonial from "@components/containers/featuresTestimonial";
@@ -22,12 +17,8 @@ export default function Home() {
       <Spacer type="md" />
       <Brands />
       <Spacer type="lg" />
-      <WalkThrough />
       <FeaturedTestimonial />
-      <ShowcaseFeatures />
       <Pricing />
-      <Questions />
-      <Testimonials />
       <FAQ />
       <ContactCard />
     </div>
