04df122d67c6226275274c68e60cccb71bd829c5
vaibhav18matere <vpm00718897@gmail.com>
2024-05-27 16:56:22 +0530

    stats text changed

--- landing-2/src/components/containers/statscard.tsx
+++ landing-2/src/components/containers/statscard.tsx
@@ -5,22 +5,22 @@ export const StatsCard = () => {
   const STATS = [
     {
       id: 1,
-      metric: "1,758",
+      metric: "1728",
       label: "Active Companies",
     },
     {
       id: 2,
-      metric: "325.8K",
+      metric: "7281",
       label: "Links Created",
     },
     {
       id: 3,
-      metric: "17.2M",
+      metric: "8721",
       label: "Clicks Tracked",
     },
   ];
   return (
-    <div className=" border-t bg-white/10 dark:bg-gray-800/10 py-0 shadow-[inset_10px_-50px_94px_0_rgb(199,199,199,0.2)] backdrop-blur w-full flex justify-center items-center  border-y-2 border-gray-200 dark:border-gray-700">
+    <div className="dark:bg-gray-800/10 py-0 w-full flex justify-center items-center dark:border-gray-700">
       <div className="w-full grid grid-cols-3 justify-center items-center max-w-7xl px-8 md:px-16 lg:px-32">
         {STATS.map(({ id, metric, label }) => {
           return (
@@ -30,11 +30,11 @@ export const StatsCard = () => {
             >
               <Typography
                 variant={"h2"}
-                className="font-semibold text-gray-600 dark:text-gray-300"
+                className="font-semibold dark:text-gray-300"
               >
                 {metric}
               </Typography>
-              <Typography className="uppercase line-clamp-1 font-semibold text-gray-500 dark:text-gray-400">
+              <Typography className="line-clamp-1 font-semibold dark:text-gray-400">
                 {label}
               </Typography>
             </div>
