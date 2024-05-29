60e4be524e4de5db35249d545b195d671191b4fe
vaibhav18matere <vpm00718897@gmail.com>
2024-05-29 13:19:02 +0530

    🎨font added

--- landing-4/src/app/layout.tsx
+++ landing-4/src/app/layout.tsx
@@ -1,11 +1,9 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { GeistSans } from "geist/font/sans";
 import "./globals.css";
 import Header from "@/components/containers/header";
 import { TailwindIndicator } from "@/components/ui/tailwindIndicator";
 import Footer from "@/components/containers/footer";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Probably",
@@ -19,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={GeistSans.className}>
         <Header />
         <TailwindIndicator />
         {children}
