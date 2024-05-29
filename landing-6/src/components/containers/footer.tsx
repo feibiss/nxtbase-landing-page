d8a4919e165d6918183f8e01258ca6f82606c4cf
Mohini Mahato <mohinimahatowork@gmail.com>
2024-05-29 21:59:14 +0530

    📦NEW: Header + Footer Updated

--- landing-6/src/components/containers/footer.tsx
+++ landing-6/src/components/containers/footer.tsx
@@ -4,58 +4,65 @@ import { Typography } from "../ui/typography";
 import { Input } from "../ui/input";
 import { Label } from "../ui/label";
 import { FOOTER_LINKS, FooterCategory } from "@/lib/enums";
+import { CubeIcon } from "@radix-ui/react-icons";
 
 export default function Footer() {
   return (
-    <footer className="flex flex-col mx-auto justify-start items-center relative bg-gray-950 px-8 md:pt-8 lg:pt-8">
-      <div className=" max-w-7xl xl:max-w-[1400px]">
-        <div className="grid grid-cols-1 h-full gap-8 lg:grid-cols-3 lg:gap-10">
-          <div className="order-2 col-span-1 flex flex-wrap justify-start lg:justify-normal items-start w-full content-start gap-4 pt-12 md:order-2 md:mx-auto md:mt-8 md:gap-8 lg:col-span-2  lg:mt-0">
-            {FOOTER_LINKS.map((details) => {
-              return (
-                <FooterCategory key={details.id} footerDetails={details} />
-              );
-            })}
+    <footer className="flex flex-col justify-start relative bg-gray-950 px-8 md:pt-8  pt-8">
+   
+        
+      <div className="grid grid-cols-1 h-full gap-8 lg:grid-cols-2 lg:gap-10">
+      <div className="col-span-1   mx-auto w-full space-y-4 pt-16 md:px-10 md:pt-8 lg:pt-12">
+          <div className="space-y-2 text-start">
+            <Typography variant={"h5"} className=" text-gray-200">
+              Subscribe to our Newsletter
+            </Typography>
+            <Typography className=" text-gray-400">
+              Stay updated with our latest news, articles, and updates.
+            </Typography>
           </div>
-          <div className="col-span-1 lg:order-2 order-1 mx-auto w-full space-y-4 pt-16 md:px-10 md:pt-8 lg:pt-12">
-            <div className="space-y-2 text-start">
-              <Typography variant={"h5"} className=" text-gray-200">
-                Subscribe to our Newsletter
-              </Typography>
-              <Typography className=" text-gray-400">
-                Stay updated with our latest news, articles, and updates.
-              </Typography>
+          <form className="space-y-4">
+            <div className="space-y-2">
+              <Label className="text-gray-300" htmlFor="email">
+                Email
+              </Label>
+              <Input
+                className=" border-gray-600 bg-gray-800 placeholder:text-gray-100"
+                id="email"
+                placeholder="Enter your email"
+                required
+                type="email"
+              />
             </div>
-            <form className="space-y-4">
-              <div className="space-y-2">
-                <Label className="text-gray-300" htmlFor="email">
-                  Email
-                </Label>
-                <Input
-                  className=" border-gray-600 bg-gray-800 placeholder:text-gray-100"
-                  id="email"
-                  placeholder="Enter your email"
-                  required
-                  type="email"
-                />
-              </div>
-              <Button variant={"default"} className="w-full" type="submit">
-                Subscribe
-              </Button>
-            </form>
-          </div>
+            <Button  className="w-full py-0 bg-fuchsia-50 text-dark hover:text-white" type="submit">
+              Subscribe
+            </Button>
+          </form>
         </div>
-        <div className="overflow-x-none mx-auto mt-8 w-full border-t-2 border-gray-700 py-8 md:mt-0">
-          <Typography variant={"mutedText"}>
-            Disclaimer: This is just a template website and is not endorsed by
-            or affiliated with any Corporation, registered in the U.S. and other
-            countries.
-          </Typography>
-          <Typography variant={"mutedText"}>
-            Copyright © 2023 NextBase. All rights reserved.
-          </Typography>
+        <div className="col-span-1 flex flex-wrap justify-start lg:justify-normal items-start w-full content-start gap-4 pt-4 md:pt-12 md:mx-auto md:mt-8 md:gap-8 lg:col-span-1 lg:mt-0">
+          {FOOTER_LINKS.map((details) => {
+            return <FooterCategory key={details.id} footerDetails={details} />;
+          })}
         </div>
+        
       </div>
+      <div className="overflow-x-none mt-8 w-full border-t-2 border-gray-700 py-8 md:mt-0  lg:px-8 text-end">
+      
+        <Typography variant={"mutedText"} className="flex items-end justify-end ">
+          Copyright © 2024 NextBase. All rights reserved.
+        </Typography>
+      </div>
+
+      <div className="flex  mx-auto flex-col justify-center items-center text-center">
+        <div className="flex flex-col items-center justify-center text-center space-y-2">
+          <Typography variant="h1" >
+           
+          </Typography>
+          <h1 className="font-bold text-[#F8FAFC] text-4xl md:text-[205.35px] md:leading-[211.98px] tracking-wider ">
+          NEXTBASE
+          </h1>
+        </div>
+     </div>
     </footer>
   );
 }
@@ -66,11 +73,8 @@ const FooterCategory = ({
   footerDetails: FooterCategory;
 }) => {
   return (
-    <div className="mb-8 flex flex-1 lg:w-1/4 flex-col md:items-baseline">
-      <Typography
-        variant={"h5"}
-        className="mb-4 text-lg font-bold text-gray-100"
-      >
+    <div className="mb-8 flex flex-auto lg:w-1/4 flex-col md:items-baseline">
+      <Typography variant={"h5"} className="mb-4 text-lg text-[#94A3B8]">
         {footerDetails.title}
       </Typography>
       <ul className="flex w-fit flex-col space-y-2">
@@ -78,7 +82,7 @@ const FooterCategory = ({
           <Link
             key={title}
             href={link}
-            className="prose font-medium text-gray-500 transition-colors hover:text-gray-200"
+            className="text-lg text-[#F8FAFC] transition-colors hover:text-gray-200"
           >
             <li>{title}</li>
           </Link>
