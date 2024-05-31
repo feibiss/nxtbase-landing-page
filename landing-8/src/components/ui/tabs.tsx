10c65def677183a2a9f74aeafc27fa5f65743428
vaibhav18matere <vpm00718897@gmail.com>
2024-05-31 17:55:51 +0530

    tabs added

--- landing-8/src/components/ui/tabs.tsx
+++ landing-8/src/components/ui/tabs.tsx
@@ -29,7 +29,7 @@ const TabsTrigger = React.forwardRef<
   <TabsPrimitive.Trigger
     ref={ref}
     className={cn(
-      "inline-flex px-4 py-2 rounded-full bg-gray-300/50 dark:bg-transparent border-2 border-slate-200 dark:border-slate-800 text-gray-400 hover:border-slate-300 dark:hover:border-slate-600 dark:hover:bg-slate-500/10 font-semibold text-lg md:text-base whitespace-nowrap items-center justify-center ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-primary dark:data-[state=active]:bg-slate-100 hover:data-[state=active]:bg-primary-hover dark:hover:data-[state=active]:bg-slate-100/90 data-[state=active]:text-slate-100 dark:data-[state=active]:text-slate-900 data-[state=active]:shadow-sm",
+      "inline-flex px-4 py-2 dark:bg-transparent border-1 dark:border-slate-800 text-gray-400 hover:border-slate-300 dark:hover:border-slate-600 dark:hover:bg-slate-500/10 font-semibold text-lg md:text-base whitespace-nowrap items-center justify-center ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-primary dark:data-[state=active]:bg-slate-100 hover:data-[state=active]:bg-primary-hover dark:hover:data-[state=active]:bg-slate-100/90 data-[state=active]:text-slate-100 dark:data-[state=active]:text-slate-900 data-[state=active]:shadow-sm",
       className
     )}
     {...props}
